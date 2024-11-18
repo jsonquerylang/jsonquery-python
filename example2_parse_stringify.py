96639808ee96f6ba3d8a62343a97bc4e7b401a1d
Jos de Jong <wjosdejong@gmail.com>
2024-11-18 12:04:34 +0100

    docs: move the examples to the root in order to make them work

---
+++ example2_parse_stringify.py
@@ -0,0 +1,33 @@
+from pprint import pprint
+from jsonquery import parse, stringify
+
+# parse the human friendly text format into the corresponding JSON format
+text_query = '.friends | filter(.city == "new York") | sort(.age) | pick(.name, .age)'
+json_query = parse(text_query)
+pprint(json_query)
+# ['pipe',
+#  ['get', 'friends'],
+#  ['filter', ['eq', ['get', 'city'], 'New York']],
+#  ['sort', ['get', 'age']],
+#  ['pick', ['get', 'name'], ['get', 'age']]]
+
+# stringify the JSON format into the corresponding text format
+print(stringify(json_query))
+# """
+# .friends
+#   | filter(.city == "new York")
+#   | sort(.age)
+#   | pick(.name, .age)
+# """
+
+# stringify with a custom indentation of 4 spaces
+print(stringify(json_query, {"indentation": "    "}))
+# """
+# .friends
+#     | filter(.city == "new York")
+#     | sort(.age)
+#     | pick(.name, .age)
+# """
+
+# now, both text_query and json_query can be used in the jsonquery() function,
+# and json_query can be used in the compile() function.
---
+++ example3_compile.py
@@ -0,0 +1,7 @@
+from jsonquery import compile
+
+# use the function `compile` to compile once and execute repeatedly
+query = ["sort"]
+execute = compile(query)
+print(execute([32, 19, 23]))  # [19, 23, 32]
+print(execute([5, 2, 7, 4]))  # [2, 4, 5, 7]
